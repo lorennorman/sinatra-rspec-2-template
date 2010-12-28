5df1f12787fed56c96569f40969f4b64b696c33e
Zachary Scott <zachary.s.scott@gmail.com>
2010-12-27 23:39:28 -0500

    its really an example

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,9 @@ Bundler.setup
 require 'rack/test'
 require_relative '../app.rb'
 
-module RSpecMixin
+module RSpecMixinExample
   include Rack::Test::Methods
   def app() Sinatra::Application end
 end
 
-RSpec.configure { |c| c.include RSpecMixin }
+RSpec.configure { |c| c.include RSpecMixinExample }
