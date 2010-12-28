7f0ef043638ecf0e54470c689ecd949efac0b422
Zachary Scott <zachary.s.scott@gmail.com>
2010-12-27 23:40:12 -0500

    simpler features

--- features/my_page.feature
+++ features/my_page.feature
@@ -1,5 +1,8 @@
-Feature: view my page
+Feature: View my page
+  In order for visitors to feel welcome
+  We must go out of our way
+  With a kind greeting
 
   Scenario: My page
-    Given I am viewing '/'
+    Given I am viewing my page 
     Then I should see "Welcome to my page!"
--- features/step_definitions/view_my_page_steps.rb
+++ features/step_definitions/view_my_page_steps.rb
@@ -1,7 +1,7 @@
-Given /^I am viewing "(.+)"$/ do |url|
-  visit(url)
+Given /^I am viewing my page$/ do
+  visit('/')
 end
 
-Then /^I should see "(.+)"$/ do |text|
-  response_body.should =~ Regexp.new(Regexp.escape(text))
+Then /^I should see "([^"]*)"$/ do |text|
+   last_response.body.should match(/#{text}/m)
 end
